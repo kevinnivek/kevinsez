0df7941689969289b1167cd357627d4000c4f9d8
Kevin Kutzko <k@kevinkevin.com>
2015-02-14 18:21:18 -0500

    major overhaul of layout, categories and tab nav

---
+++ www/js/kevinsez.js
@@ -0,0 +1,244 @@
+angular.module('kevnav', ['ionic'])
+
+// for media plugin : http://plugins.cordova.io/#/package/org.apache.cordova.media
+.factory('MediaSrv', function($q, $ionicPlatform, $window) {
+         var service = {
+         loadMedia: loadMedia,
+         getStatusMessage: getStatusMessage,
+         getErrorMessage: getErrorMessage
+         };
+         
+         function loadMedia(src, onError, onStatus, onStop) {
+         var defer = $q.defer();
+         $ionicPlatform.ready(function() {
+                              var mediaSuccess = function() {
+                              if (onStop) {
+                              onStop();
+                              }
+                              };
+                              var mediaError = function(err) {
+                              _logError(src, err);
+                              if (onError) {
+                              onError(err);
+                              }
+                              };
+                              var mediaStatus = function(status) {
+                              if (onStatus) {
+                              onStatus(status);
+                              }
+                              };
+                              
+                              if ($ionicPlatform.is('android')) {
+                              src = '/android_asset/www/' + src;
+                              }
+                              defer.resolve(new $window.Media(src, mediaSuccess, mediaError, mediaStatus));
+                              });
+         return defer.promise;
+         }
+         
+         function _logError(src, err) {
+         console.error('media error', {
+                       code: err.code,
+                       message: getErrorMessage(err.code)
+                       });
+         }
+         
+         function getStatusMessage(status) {
+         if (status === 0) {
+         return 'Media.MEDIA_NONE';
+         } else if (status === 1) {
+         return 'Media.MEDIA_STARTING';
+         } else if (status === 2) {
+         return 'Media.MEDIA_RUNNING';
+         } else if (status === 3) {
+         return 'Media.MEDIA_PAUSED';
+         } else if (status === 4) {
+         return 'Media.MEDIA_STOPPED';
+         } else {
+         return 'Unknown status <' + status + '>';
+         }
+         }
+         
+         function getErrorMessage(code) {
+         if (code === 1) {
+         return 'MediaError.MEDIA_ERR_ABORTED';
+         } else if (code === 2) {
+         return 'MediaError.MEDIA_ERR_NETWORK';
+         } else if (code === 3) {
+         return 'MediaError.MEDIA_ERR_DECODE';
+         } else if (code === 4) {
+         return 'MediaError.MEDIA_ERR_NONE_SUPPORTED';
+         } else {
+         return 'Unknown code <' + code + '>';
+         }
+         }
+         
+         return service;
+         })
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+        
+        $stateProvider
+        .state('tabs', {
+               url: "/tab",
+               abstract: true,
+               templateUrl: "templates/tabs.html"
+               })
+        .state('tabs.home', {
+               url: "/home",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/home.html",
+               controller: 'HomeTabCtrl'
+               }
+               }
+               })
+        // actual categories START
+        .state('tabs.bodyslimes', {
+               url: "/bodyslimes",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/bodyslimes.html"
+               }
+               }
+               })
+        .state('tabs.jammingfingers', {
+               url: "/jammingfingers",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/jammingfingers.html"
+               }
+               }
+               })
+        
+        .state('tabs.feet', {
+               url: "/feet",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/feet.html"
+               }
+               }
+               })
+        .state('tabs.cleaningareas', {
+               url: "/cleaningareas",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/cleaningareas.html"
+               }
+               }
+               })
+        
+        .state('tabs.ticklecloset', {
+               url: "/ticklecloset",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/ticklecloset.html"
+               }
+               }
+               })
+        .state('tabs.sleeping', {
+               url: "/sleeping",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/sleeping.html"
+               }
+               }
+               })
+        
+        .state('tabs.hotbreathing', {
+               url: "/hotbreathing",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/hotbreathing.html"
+               }
+               }
+               })
+        .state('tabs.watchingafar', {
+               url: "/watchingafar",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/watchingafar.html"
+               }
+               }
+               })
+        
+        .state('tabs.huffing', {
+               url: "/huffing",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/huffing.html"
+               }
+               }
+               })
+        .state('tabs.warmsmiles', {
+               url: "/warmsmiles",
+               views: {
+               'home-tab': {
+               templateUrl: "templates/warmsmiles.html"
+               }
+               }
+               })
+        
+        // actual categories END
+        .state('tabs.about', {
+               url: "/about",
+               views: {
+               'about-tab': {
+               templateUrl: "templates/about.html"
+               }
+               }
+               })
+        .state('tabs.navstack', {
+               url: "/navstack",
+               views: {
+               'about-tab': {
+               templateUrl: "templates/nav-stack.html"
+               }
+               }
+               })
+        .state('tabs.contact', {
+               url: "/contact",
+               views: {
+               'contact-tab': {
+               templateUrl: "templates/contact.html"
+               }
+               }
+               });
+        
+        
+        $urlRouterProvider.otherwise("/tab/home");
+        
+        })
+
+.controller('HomeTabCtrl', function($scope) {
+            console.log('HomeTabCtrl');
+            
+            })
+
+.controller('AudioCtrl', function($scope, MediaSrv) {
+            $scope.playAudio = function(filename) {
+            //  alert("test");
+            MediaSrv.loadMedia('sounds/' + filename).then(function(media) {
+                                                          media.play();
+                                                          });
+            }
+            $scope.playRandAudio = function(filename) {
+            var chooser = randomNoRepeats(['areas-of-my-body.mp3', 'body-examined.mp3', 'bradley-pussywillow.mp3', 'dont-enter-bedroom.mp3', 'father-foot-inspector.mp3', 'feet-slime.mp3', 'inspect-your-feet.mp3', 'lieing-down-sleeping.mp3', 'secret-room-cellar.mp3', 'stool-examined.mp3', 'toe-jobs.mp3', 'touch-left-hand-only.mp3', 'various-slimes.mp3']);
+            MediaSrv.loadMedia('sounds/' + chooser()).then(function(media) {
+                                                           media.play();
+                                                           });
+            }
+            function randomNoRepeats(array) {
+            var copy = array.slice(0);
+            return function() {
+            if (copy.length < 1) { copy = array.slice(0); }
+            var index = Math.floor(Math.random() * copy.length);
+            var item = copy[index];
+            copy.splice(index, 1);
+            return item;
+            };
+            }
+            
+            
+            });
